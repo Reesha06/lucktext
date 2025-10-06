e866cc0a2f5467698fa46f8fde847427f6f83700
Reesha <reeshasaeed30@gmail.com>
2025-10-06 11:00:19 +0200

    nh fnh fhn fy

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Vad heter du?");
+string name = Console.ReadLine();
+
+string place = Console.ReadLine();
+string thing = Console.ReadLine();
+
+Console.WriteLine("Hej Läraren");
+Console.WriteLine($" {name} är sjuk och därför kan inte komma till {place} idag. Kan du snälla anmäla frånvaro åt henne. Hon glömde sin {thing} i skolan");
+
+
+Console.ReadLine();
+
+Console.WriteLine("Skirv en siffra:");
+string s = Console.ReadLine();
+int siffra; 
